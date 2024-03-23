fe568ff612331069ea3966d8982d51b8d8bac454
Alpesh SHETH <106927885+ASheth-Git@users.noreply.github.com>
2024-03-23 12:15:26 +0100

    Update Mat_Freq_Gen.py
    Re-Licensing to Apache

--- Mat_Freq_Gen.py
+++ Mat_Freq_Gen.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # MIT License
 # 
 # Copyright (c) 2024 Alpesh Rasikbhai SHETH
@@ -19,6 +20,22 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+=======
+"""
+Copyright 2024 Alpesh Rasikbhai SHETH
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+>>>>>>> parent of b41a208 (Update Mat_Freq_Gen.py)
 
 # =============================================================================
 # Matsubara Frequency Generator
